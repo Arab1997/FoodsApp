cfbd5a09b11eb72bcb39e4c3bc6cb7c0e7bad538
Abdulloh <maxmudovabdullo97@gmail.com>
2020-06-26 17:54:49 +0500

    Initial commit

---
+++ app/src/main/java/com/example/foodsapp/home/HomeActivity.java
@@ -0,0 +1,48 @@
+/*-----------------------------------------------------------------------------
+ - Developed by Haerul Muttaqin                                               -
+ - Last modified 3/17/19 5:24 AM                                              -
+ - Subscribe : https://www.youtube.com/haerulmuttaqin                         -
+ - Copyright (c) 2019. All rights reserved                                    -
+ -----------------------------------------------------------------------------*/
+package com.example.foodsapp.home;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.foodsapp.R;
+
+
+// TODO 31 implement the HomeView interface at the end
+public class HomeActivity extends AppCompatActivity {
+
+    /*
+     * TODO 32 Add @BindView Annotation (1)
+     *
+     * 1. viewPagerHeader
+     * 2. recyclerCategory
+     *
+     */
+
+    /*
+     *  TODO 33 Create variable for presenter;
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        /*
+         *  TODO 34 bind the ButterKnife (2)
+         */
+
+        /*
+         *  TODO 35 Declare the presenter
+         *  new HomePresenter(this)
+         */
+    }
+
+    // TODO 36 Overriding the interface
+
+}
---
+++ app/src/main/java/com/example/foodsapp/model/Meals.java
@@ -0,0 +1,16 @@
+/*-----------------------------------------------------------------------------
+ - Developed by Haerul Muttaqin                                               -
+ - Last modified 3/17/19 5:24 AM                                              -
+ - Subscribe : https://www.youtube.com/haerulmuttaqin                         -
+ - Copyright (c) 2019. All rights reserved                                    -
+ -----------------------------------------------------------------------------*/
+package com.example.foodsapp.model;
+
+public class Meals {
+
+
+
+    public class Meal {
+
+    }
+}
